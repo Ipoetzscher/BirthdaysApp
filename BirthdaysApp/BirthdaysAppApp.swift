d822188acbdf94d53945ae704201f2e02b85587a
Ipoetzscher <ikpjnk@gmail.com>
2025-04-26 09:24:40 -0700

    Initial Commit

---
+++ BirthdaysApp/BirthdaysAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysAppApp.swift
+//  BirthdaysApp
+//
+//  Created by India Poetzscher on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BirthdaysAppTests/BirthdaysAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysAppTests.swift
+//  BirthdaysAppTests
+//
+//  Created by India Poetzscher on 4/26/25.
+//
+
+import Testing
+@testable import BirthdaysApp
+
+struct BirthdaysAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
