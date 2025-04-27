754b6d6132239ac5532aa887c687bd8b49204315
Ipoetzscher <ikpjnk@gmail.com>
2025-04-27 10:36:44 -0700

    Added UI for birthdays app

---
+++ BirthdaysApp/Friend.swift
@@ -0,0 +1,18 @@
+//
+//  Friend.swift
+//  BirthdaysApp
+//
+//  Created by India Poetzscher on 4/27/25.
+//
+
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+}
