0507958b5d3923367d6d33f091454144d7944895
Ipoetzscher <ikpjnk@gmail.com>
2025-04-27 11:11:01 -0700

    added update friend functionality

--- BirthdaysApp/ContentView.swift
+++ BirthdaysApp/ContentView.swift
@@ -17,22 +17,30 @@ struct ContentView: View {
     @Environment(\.modelContext) private var context
     @State private var newName = ""
     @State private var newBirthday = Date.now
+    @State private var selectedFriend: Friend?
     
     var body: some View {
         NavigationStack {
             List(friends) { friend in
                 ForEach(friends) { friend in
                     HStack {
-                        HStack {
-                            Text(friend.name)
-                            Spacer()
-                            Text(friend.birthday, format: .dateTime.month(.wide).day().year())
-                        }
+                        Text(friend.name)
+                        Spacer()
+                        Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                    }
+                    .onTapGesture {
+                        selectedFriend = friend
                     }
                 }
                 .onDelete(perform: deleteFriend)
+                
             }
             .navigationTitle("Birthdays")
+            .sheet(item: $selectedFriend) { friend in
+                NavigationStack { // new
+                    EditFriendView(friend: friend)
+                }
+            }
             .safeAreaInset(edge: .bottom) {
                 VStack(alignment: .center, spacing: 20) {
                     Text("New Birthday")
